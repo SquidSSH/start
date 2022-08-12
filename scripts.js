fa22d6817be19607a974b58c0f1d4c5a663d9449
Squ1d <81778609+SquidSSH@users.noreply.github.com>
2022-08-12 15:10:24 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -14,7 +14,7 @@ const engineUrls = {
   deepl: "https://www.deepl.com/translator#-/-/",
   duckduckgo: "https://duckduckgo.com/?q=",
   ecosia: "https://www.ecosia.org/search?q=",
-  google: "https://www.google.com/search?q=",
+  searx: "https://searx.priv.pw/search?q=",
   startpage: "https://www.startpage.com/search?q=",
   youtube: "https://www.youtube.com/results?q=",
 }
